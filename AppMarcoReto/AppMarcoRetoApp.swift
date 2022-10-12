2713e7a484960479337f82677d7b1fae68e36f60
César <a01197841@tec.mx>
2022-10-12 12:33:34 -0500

    Initial Commit

---
+++ AppMarcoReto/AppMarcoRetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMarcoRetoApp.swift
+//  AppMarcoReto
+//
+//  Created by Alumno on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AppMarcoRetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppMarcoReto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppMarcoReto
+//
+//  Created by Alumno on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
