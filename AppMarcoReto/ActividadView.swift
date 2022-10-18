e5e99509fa01d2e290c3130cd3453b13dfaf1754
César <a01197841@tec.mx>
2022-10-18 12:22:32 -0500

    Diseño basico de la parte interactiva de la aplicación. Incluye botones, textos y una imagen. Los botones no son funcionales.

--- AppMarcoReto/ActividadView.swift
+++ AppMarcoReto/ActividadView.swift
@@ -9,7 +9,73 @@ import SwiftUI
 
 struct ActividadView: View {
     var body: some View {
-        Text("Actividad Interactiva")
+            VStack{
+                Text("Puntos: 0")
+                    .padding()
+                    .offset(x: -125)
+                Text("¿Cual es el nombre de esta obra?")
+                Image("CasinoFilosofico")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                Button(action: {
+                    print("Respuesta Equivocada")
+                    }, label: {
+                        Text("Dados Filosoficos")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width:350, height: 75)
+                            .background(Color.brown)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            
+                    })
+                
+                Button(action: {
+                    print("Respuesta Equivocada")
+                    }, label: {
+                        Text("Filosofia en lo Azar")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width:350, height: 75)
+                            .background(Color.brown)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            
+                    })
+                Button(action: {
+                    print("Respuesta Equivocada")
+                    }, label: {
+                        Text("Apuesta Filosofica")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width:350, height: 75)
+                            .background(Color.brown)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            
+                    })
+                Button(action: {
+                    print("Respuesta Correcta")
+                    }, label: {
+                        Text("Casino Filosofico")
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width:350, height: 75)
+                            .background(Color.brown)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                                                })
+                .padding(.bottom, 60)
+                
+                
+                
+            }
     }
 }
 
