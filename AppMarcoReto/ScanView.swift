c8a94d518aa3fec585981eb0bdf698c42a74dec4
Emmanuel Granados <76198594+sporrk12@users.noreply.github.com>
2022-10-18 14:35:30 -0500

    Realidad aumentada agregada a vista de Scan

--- AppMarcoReto/ScanView.swift
+++ AppMarcoReto/ScanView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import ARKit
+import RealityKit
+
+struct RealityKitView: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARView {
+       let view = ARView()
+        
+        // Start AR session
+        let session = view.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        session.run(config)
+        
+        // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        view.addSubview(coachingOverlay)
+        
+        // Set debug options
+        #if DEBUG
+        view.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
+        
+       return view
+    }
+
+    func updateUIView(_ view: ARView, context: Context) {
+    }
+}
 
 struct ScanView: View {
     var body: some View {
-        Text("Scan")
+        RealityKitView()
     }
 }
 
