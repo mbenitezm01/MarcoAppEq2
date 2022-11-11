3ef91be3feee98da44246c338f4a14b85d0d7ed3
RKipper2000 <A01283516@itesm.mx>
2022-11-11 11:09:26 -0600

    NavigationLink Fix

--- AppMarcoReto/HomeView.swift
+++ AppMarcoReto/HomeView.swift
@@ -8,37 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var obrasVM = ObrasViewModel()
     var body: some View {
-        VStack{
-            Text("Expos")
-                .font(Font.custom("MarkPro-Book",size: 36))
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) { // HStack(spacing:20)
-                    ForEach(1..<4) {
-                        Text("Obra \($0)")
-                            .foregroundColor(.white)
-                            .font(.largeTitle)
-                            .frame(width: 200, height: 200)
-                            .background(.red)
-                    } // ForEach
-                } // HStack
-            } // ScrollView
-            Text("Eventos")
-                .font(Font.custom("MarkPro-Book",size: 36))
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) {
-                    ForEach(1..<4) {
-                        Text("Evento \($0)")
-                            .font(Font.custom("MarkPro-Book",size: 36))
-                            .foregroundColor(.white)
-                            .font(.largeTitle)
-                            .frame(width: 200, height: 200)
-                            .background(.red)
-                    } // ForEach
-                } // HStack
-            } // ScrollView
-        } // VStack
-        
+        NavigationView{
+            ZStack{
+                Color("ColorFondoMarco")
+                    .ignoresSafeArea()
+                VStack{
+                    Text("Expos")
+                        .font(Font.custom("MarkPro-Book",size: 36))
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) { // HStack(spacing:20)
+                            ForEach(obrasVM.arrObras) { item in NavigationLink(destination:ObrasDetailView(obra: item),label:{ObrasItemView(obra:item)})
+                                /*Text("Obra \($0)")
+                                 .foregroundColor(.white)
+                                 .font(.largeTitle)
+                                 .frame(width: 200, height: 200)
+                                 .background(.red)*/
+                            } // ForEach
+                        } // HStack
+                    } // ScrollView
+                    Text("Eventos")
+                        .font(Font.custom("MarkPro-Book",size: 36))
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            ForEach(1..<4) {
+                                Text("Evento \($0)")
+                                    .font(Font.custom("MarkPro-Book",size: 36))
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                    .frame(width: 200, height: 200)
+                                    .background(.red)
+                            } // ForEach
+                        } // HStack
+                    } // ScrollView
+                    
+                } // VStack
+            }//ZStack
+        }//NavigationView
     }
 }
 
--- AppMarcoReto/Obras/ObrasDetailView.swift
+++ AppMarcoReto/Obras/ObrasDetailView.swift
@@ -12,36 +12,51 @@ struct ObrasDetailView: View {
     let obra:ObrasModel
     
     var body: some View {
-        VStack{
-            Image(obra.nombreImagen)
-                .resizable()
-                .scaledToFit()
-                .frame(width:250)
-                .clipShape(Rectangle())
-                .shadow(radius:7)
-                .offset(y:-100)
-            
-            Text(obra.titulo)
-                .font(Font.custom("MarkPro-Book", size: 65))
-                .offset(y:-100)
-            
-            Text("\(obra.artista), \(String(obra.year))")
-                .font(Font.custom("MarkPro-Book", size: 20))
-                .offset(y:-100)
-            
-            Text("Tecnica: \(String(obra.tecnica))")
-                .font(Font.custom("MarkPro-Book", size: 20))
-                .offset(y:-100)
-            
-            Text("Medidas: \(String(obra.medidas))")
-                .font(Font.custom("MarkPro-Book", size: 20))
-                .offset(y:-100)
-            Text("\n\(obra.descripcion)")
-                .font(Font.custom("MarkPro-Book", size: 20))
-                .offset(y:-100)
-                .multilineTextAlignment(.center)
-            Link(destination: URL(string:obra.linkVideo)!, label: {Label("Video", systemImage: "play.tv.fill")})
-        }//VStack
+        ZStack{
+            Color("ColorFondoMarco")
+                .ignoresSafeArea()
+            VStack{
+                Image(obra.nombreImagen)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:250)
+                    .clipShape(Rectangle())
+                    .shadow(radius:7)
+                    .offset(y:-50)
+                
+                Text(obra.titulo)
+                    .font(Font.custom("MarkPro-Book", size: 40))
+                    .offset(y:-50)
+                
+                Text("\(obra.artista), \(String(obra.year))")
+                    .font(Font.custom("MarkPro-Book", size: 20))
+                    .offset(y:-50)
+                
+                Text("Tecnica: \(String(obra.tecnica))")
+                    .font(Font.custom("MarkPro-Book", size: 20))
+                    .offset(y:-50)
+                
+                Text("Medidas: \(String(obra.medidas))")
+                    .font(Font.custom("MarkPro-Book", size: 20))
+                    .offset(y:-50)
+                Text("\n\(obra.descripcion)")
+                    .font(Font.custom("MarkPro-Book", size: 20)
+                        )
+                    .padding(.horizontal, 20)
+                    .offset(y:-50)
+                    .multilineTextAlignment(.center)
+                Link(destination: URL(string:obra.linkVideo)!, label: {Label("Video", systemImage: "play.tv.fill")})
+                
+                HStack{
+                    Text("Quiz")
+                        .background(.brown)
+                        .foregroundColor(.white)
+                    Text("3D")
+                        .background(.brown)
+                        .foregroundColor(.white)
+                }//HStack
+            }//VStack
+        }//ZStack
     }
 }
 
