d07a773cc0013164e428e7017a3462ac7e7f6f02
Emmanuel Granados <76198594+sporrk12@users.noreply.github.com>
2022-11-18 12:48:54 -0600

    Seleccion de posicion de modelo utilizando taps

--- AppMarcoReto/ScanView.swift
+++ AppMarcoReto/ScanView.swift
@@ -8,43 +8,16 @@
 import SwiftUI
 import ARKit
 import RealityKit
-import Combine
-
-/*
-struct RealityKitView: UIViewRepresentable {
-    func makeUIView(context: Context) -> ARView {
-       let view = ARView()
-        
-        // Start AR session
-        let session = view.session
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal]
-        session.run(config)
-        
-        // Add coaching overlay
-        let coachingOverlay = ARCoachingOverlayView()
-        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        coachingOverlay.session = session
-        coachingOverlay.goal = .horizontalPlane
-        view.addSubview(coachingOverlay)
-        
-        // Set debug options
-        #if DEBUG
-        view.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
-        #endif
-        
-       return view
-    }
-
-    func updateUIView(_ view: ARView, context: Context) {
-    }
-}
- */
-
 
 struct ScanView: View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
+        .safeAreaInset(edge: .bottom, alignment: .center, spacing: 0) {
+                Color.clear
+                    .frame(height: 0)
+                    .background(Material.bar)
+            }
+            .ignoresSafeArea(.all, edges: .top)
     }
 }
 
