56a4bc6c9c6af16527e5922bcd628a2d6615c8b2
Marcelo Benitez <a01721168@tec.mx>
2022-12-01 14:50:49 -0600

    Merge

--- AppMarcoReto/ARViewController.swift
+++ AppMarcoReto/ARViewController.swift
@@ -10,15 +10,28 @@ import ARKit
 import RealityKit
 import SwiftUI
 import Combine
+import FocusEntity
 
 class ARViewController: UIViewController, ARSessionDelegate, ARCoachingOverlayViewDelegate {
-
+                
     var modelName:String = ""
-
+            
+    var placeMode:Bool = true
+    
+    var focusArr:[FocusEntity] = []
+    
+    func setMode(mode:Bool){
+        placeMode = mode
+        if placeMode == true{
+            focusArr[0].isEnabled = true
+        }
+        else { focusArr[0].isEnabled = false }
+    }
+    
     public func updateModelName(name:String){
         modelName = name
     }
-
+    
     private var arView: ARView!
 
     override func viewDidLoad() {
@@ -26,115 +39,110 @@ class ARViewController: UIViewController, ARSessionDelegate, ARCoachingOverlayVi
         // Do any additional setup after loading the view.
         arView = ARView(frame: view.bounds)
         view.addSubview(arView)
-
+        let focusSquare = FocusEntity(on: self.arView, focus: .classic)
+        focusArr.append(focusSquare)
+        
         // Set debug options
-        #if DEBUG
-        arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
-        #endif
+        //#if DEBUG
+        //arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        //#endif
 
     }
-
+    
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
-
+        
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal]
-
-
+        
+        
         arView.session.run(config)
         arView.session.delegate = self
-
-        //placeModel()
-
-
-
+        
+        placeModel(modelName: modelName)
+        
     }
-
-    /*
-
-
-    func placeModel() {
-        guard let model = try? Entity.load(named: "model") else { return }
-        model.name = "model"
-
-        let anchor = AnchorEntity(plane: .horizontal)
-        anchor.addChild(model)
-
-        arView.scene.addAnchor(anchor)
-
-        print("place model")
-
-    }
-     */
-
-    func moveModel(raycast: ARRaycastResult) {
-
-        arView.scene.findEntity(named: "model")?.removeFromParent()
-
+    
+    
+     
+     
+    func placeModel(modelName:String) {
         guard let model = try? Entity.load(named: modelName) else { return }
         model.name = "model"
-
-
-        let anchor = AnchorEntity(raycastResult: raycast)
-
+        
+        let anchor = AnchorEntity(plane: .horizontal)
         anchor.addChild(model)
-
+        
         arView.scene.addAnchor(anchor)
-
-
+        
+        print("place model")
+        
     }
-
-
+     
+    
+    func moveModel(transform: simd_float4x4) {
+        
+        guard let model = arView.scene.findEntity(named: "model") else { return }
+        
+        model.move(to: transform, relativeTo: nil)
+        
+        
+    }
+    
+    
     func addTapGesture() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         self.arView.addGestureRecognizer(tap)
     }
-
+    
     @objc func handleTap(recognizer: UITapGestureRecognizer?) {
-
+        
         print("tap")
-
+        
         let results = self.arView.raycast(from: self.arView.center, allowing: .existingPlaneGeometry, alignment: .horizontal)
         if let firstResult = results.first {
-
-            print("move model")
-            moveModel(raycast: firstResult)
-
+            
+            if placeMode {
+                print("move model")
+                moveModel(transform: firstResult.worldTransform)
+            }
+            
         }
     }
-
-
-
+        
+        
+    
     override func viewDidAppear(_ animated: Bool) {
         addTapGesture()
         super.viewDidAppear(animated)
         print("viewDidAppear")
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         print("viewWillDisappear")
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         print("viewDidDisappear")
     }
-
+    
 
 }
 
 extension ARViewController {
-
+    
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]){
         print("didAdd")
     }
-
+    
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]){
         //print("didUpdate")
     }
-
+    
     func session(_ session: ARSession, didRemove anchors: [ARAnchor]){
+        //focusSquare.isEnabled = false
         print("didRemove")
     }
 }
--- AppMarcoReto/ScanView.swift
+++ AppMarcoReto/ScanView.swift
@@ -12,28 +12,46 @@ import RealityKit
 struct ScanView: View {
     let modelName:String
     
+    @State var toggleState = true
+            
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
-        .safeAreaInset(edge: .bottom, alignment: .center, spacing: 0) {
-                Color.clear
-                    .frame(height: 0)
-                    .background(Material.bar)
+        ZStack{
+            
+            ARViewContainer(modelName: modelName, toggleState: toggleState).edgesIgnoringSafeArea(.all)
+                .safeAreaInset(edge: .bottom, alignment: .center, spacing: 0) {
+                    Color.clear
+                        .frame(height: 0)
+                        .background(Material.bar)
+                }
+                .ignoresSafeArea(.all, edges: .top)
+            VStack{
+                Spacer()
+                HStack{
+                    Toggle("Mover Modelo", isOn: $toggleState)
+                        .toggleStyle(.button)
+                        .padding()
+                    Spacer()
+                }
             }
-            .ignoresSafeArea(.all, edges: .top)
+            
+        }
     }
 }
 
 struct ARViewContainer: UIViewControllerRepresentable {
+    let modelName:String
+    let toggleState:Bool
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ARViewContainer>) -> ARViewController{
-
+        
         let viewController = ARViewController()
-        viewController.updateModelName(name: "gramophone")
+        viewController.updateModelName(name: modelName)
         return viewController
-
+        
     }
-
+    
     func updateUIViewController(_ uiViewController: ARViewController, context: UIViewControllerRepresentableContext<ARViewContainer>){
-
+        uiViewController.setMode(mode: toggleState)
     }
 }
 
